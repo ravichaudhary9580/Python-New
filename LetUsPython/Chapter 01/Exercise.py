8718cc163ebab3c3f4091924b8ef4f14eff1e2d9
Ravi Chaudhary <ravichaudhary9580@outlook.com>
2025-03-07 11:17:43 +0530

    Update1

---
+++ LetUsPython/Chapter 01/Exercise.py
@@ -0,0 +1,47 @@
+[A] Answer the following:
+(a) Mention 5 fields in which Python  is popularly used.
+# 1- System Programming 
+# 2- Building GUI Application
+# 3- Console Application 
+# 4- Robotics Programming 
+# 5- Numeric and Scientific Programming
+(b) Where is event - driven programming popularly used ? 
+# This programming pardigms popularly used in GUI applications containing windows, checkboxes, scrollbars, buttons etc
+(c) Why Python is called Portable language?
+# Because python create byte code from source code and it can be used by different PMVs as per the OS and execute it that's why Python is portable language.
+(d) What is the single most important feature of different programming models disscussed in this chapter?
+# Object - Oriented Programming Model is the single most important feature of different programming models discussed in this chapter.
+(e) Which of the following is not a feature of Python?  
+- Static typing 
+- Dynamic Typing  
+- Run-time error handling through error numbers 
+- Library support for containers like  Lists, Dictionaries, Tuples.
+# Answer: Run-time error handling through error numbers is not a feature of Python
+(f) Give an example application of each of the following programming models:
+# Example applications for different programming models:
+ Functional Programming # Example: Data processing pipelines, mathematical computations
+ Procedural Programming # Example: Command line utilities, batch processing scripts
+ Object-Oriented Programming # Example: Game development, enterprise applications
+ Event-Driven Programming # Example: GUI applications, web applications, mobile apps
+
+[B] State whether the following statements are True or False:
+(a) Python is free to  use and distribute. {True}
+(b) Same Python program can work on different OS - microporcessor combinations. {True} 
+(c) It is possible to use C++ or Java libraries in a Python Program {True}
+(d) In Python type of the variable is decided based on its usage. {True}
+(e) Python can not be used for building GUI applications. {False}
+(f) Python supports functional, procedural, object-oriented and event-driven programming models.{True}
+(g) GUI applications are based on event-driven programming model. {True}
+(h) Functional programming model consists of interaction of multiple objects. {False}  
+
+[C] Match the following pairs:
+(a) Functional programming ---- 4 - Math-like functions
+(b) Event-driven programming ---- 1 - GUI element-based interaction
+(c) Procedural programming ---- 3 - Statments
+(d) OOP ---- 2 - Interaction of Objects
+
+[D] Fill in the blanks:
+(a) Funtional programming paradigm is also known as '''Declarative programming''' model.
+(b) Procedural programming paradigm is also known as '''Imperative programming''' model.
+(c) Python was created by '''Guido Van Rossum'''
+(d) Python programmers often called Pythonists or Pythonistas.
---
+++ LetUsPython/Chapter 02/Exercise.py
@@ -0,0 +1,39 @@
+[A] Answer the following questions.
+(a) What do the prompts C:\>, $ and >>> signify?
+# C:\> is the Windows command prompt indicating the current directory is C:\
+# $ is the Unix/Linux shell prompt indicating ready for command input
+# >>> is the Python interactive shell prompt indicating ready for Python code input
+
+(b) In which two modes can IDLE be used?
+# 1 - Interactive mode
+# 2 - Script mode
+
+(c) What is the purpose of the two programming modes offered by IDLE?
+# Interactive mode - Used for exploring python syntax, seek help and debug short program
+# Script mode used for writing full-fledged Python programs.
+
+(d) How can third party libraries be used in a python program?
+# Third party libraries can be installed using pip install command
+# Then imported into Python programs using import statement
+# Example: pip install numpy
+# Then in code: import numpy as np
+
+[B] Match the following pairs:
+(a) pip ---- 6- Package installation tool 
+(b) Jupyter ---- 7- Build and document applications
+(c) Spyder ---- 8- Scientific library
+(d) PyPi ---- 9- Python package index
+(e) NumPy ---- 1- Advance Mathematical operations
+(f) SciPy ---- 2- Scientific computing
+(g) Pandas ---- 3- Manipulate numerical tables
+(h) MatPlotLib ---- 4-Visualization
+(i) OpenCV ---- 5- Computer Vision
+
+[C] State whether the following statements are True or False:
+(a) Python is a specification that can be implemented through language like Python, C#, Java, etc. {True}
+(b) CPython is implementation of Python specification, written in C. {True}
+(c) Python program is first compiled into byte code, which is then interpreted. {True}
+(d) Most Linux distribution already contain Python.{True}
+(e) Windows system does not contain Python and it needs to separately installed. {True}
+(f) Python programs can be built using IDLE, NetBeans, PyCharm and Visual Studio Code. {True}
+(g) Third-party Python packages are distributed usin PyPI.{True}
